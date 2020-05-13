2982212845b2c1c50f09d4a3bc632c265f83aff1
Josh Blumberg <jlblumberg@gmail.com>
2020-05-13 11:09:09 +0100

    Add chart.js

--- timing.js
+++ timing.js
@@ -1,7 +1,6 @@
-const { performance } = require('perf_hooks');
-
 const inbuiltReverse = Array.prototype.reverse
 
+// Create array of arrays. Each array has random ints between 1 and 100.
 const createTestArray = () => {
   let result = [];
   for (let length = 5000; length <= 100000; length += 5000) {
@@ -12,6 +11,7 @@ const createTestArray = () => {
   return result;
 }
 
+// for calculating an average
 const average = (arr) => {
   let sum = 0;
   for (let i = 0; i < arr.length; i++) {
@@ -20,12 +20,14 @@ const average = (arr) => {
   return sum / arr.length;
 }
 
+// fun the func on the array 10 times and do nothing with it
 const tenThrowAwayRuns = (arr, fnc) => {
   for (let run = 1; run < 11; run++) {
     fnc.apply(arr);
   }
 }
 
+// do throw away runs, then time the fnc 50 times and average the result
 const measureTime = (arr, fnc) => {
   let runsToAverage = []
   tenThrowAwayRuns(arr, fnc);
@@ -38,13 +40,22 @@ const measureTime = (arr, fnc) => {
   return average(runsToAverage);
 }
 
+// run the timer on the array of arrays
 const runPerformanceTest = methodOfChoice => {
   data = {}
   let testArray = createTestArray();
   for (let i = 0; i < testArray.length; i++) {
     data[testArray[i].length] = measureTime(testArray[i], methodOfChoice)
   };
-  return [data];
+  return data;
 };
 
-console.log(runPerformanceTest(inbuiltReverse));
+// format data for chart.js
+const dataForChartJs = (fnc) => {
+  let formattedData = [];
+  let unformattedData = runPerformanceTest(fnc);
+  for (const length in unformattedData) {
+    formattedData.push({x: length, y: unformattedData[length]});
+  };
+  return formattedData;
+}
