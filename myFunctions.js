ae0a50bd462c3c7a45d8d7301313188b960f7fd0
Josh Blumberg <jlblumberg@gmail.com>
2020-06-25 11:45:14 +0100

    Start building out binary search

--- myFunctions.js
+++ myFunctions.js
@@ -2,12 +2,18 @@
 
 const myFunctions = {};
 
+// reverse
 myFunctions.myReverse = (arr) => {
   let newArr = [];
   while (arr.length > 0) {
     newArr.push(arr.pop());
   };
   return newArr;
+}
+
+// binary search
+myFunctions.myBinarySearch = (arr) => {
+ return -1
 }
 
 // check if we are in browser or mocha, only export for mocha
--- myFunctions.test.js
+++ myFunctions.test.js
@@ -1,27 +1,36 @@
-const expect = require('chai').expect;
+const { expect } = require('chai');
 const myFunctions = require('./myFunctions');
 
 describe('myReverse', () => {
   it('returns an empty array if given an empty array', () => {
     expect(myFunctions.myReverse([])).to.deep.equal([]);
-  })
+  });
 
   it('returns the given array if it\'s only one element', () => {
     expect(myFunctions.myReverse([1])).to.deep.equal([1]);
-  })
+  });
 
   it('switches the order of a two element array', () => {
     expect(myFunctions.myReverse([1, 2])).to.deep.equal([2, 1]);
-  })
+  });
 
   it('reverses a three element array', () => {
     expect(myFunctions.myReverse([1, 2, 3])).to.deep.equal([3, 2, 1]);
-  })
+  });
 
   it('reverses a longer array', () => {
     let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     let desiredResult = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
     expect(myFunctions.myReverse(arr)).to.deep.equal(desiredResult);
-  })
+  });
 
-})
+});
+
+describe('myBinarySearch', () => {
+
+  it('returns -1 if given an empty array', () => {
+    let arr = []
+    expect(myFunctions.myBinarySearch(arr)).to.equal(-1);
+  });
+
+});
